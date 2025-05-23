c6c69f7b42ca4840a0ce904824e7e088c9eadfb7
anderson <anjimhol23@gmail.com>
2025-05-23 23:20:52 +0100

    Refactor server setup to use Express instead of native http module

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hola, mundo con TypeScript!\n');
-});
-
-const PORT = 8080;
-server.listen(PORT, () => {
-console.log(`Servidor corriendo en http://localhost:${PORT}/`);
-});
-=======
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -32,4 +19,3 @@ app.use("/api",songRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
->>>>>>> 9726a4c12b76eb8d283a1fc93101182ee6b15d5c
