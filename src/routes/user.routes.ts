48d3df0c4c590069676da54f0b05eb10b29b9b98
AJimenezHolguin <122332192+AJimenezHolguin@users.noreply.github.com>
2025-05-24 07:03:09 +0100

    Refactor authentication middleware and user routes (#5)

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,4 @@
 import { Router } from "express";
-<<<<<<< HEAD
-import { deleteUser, getUsers, login, register } from "../controller/user.controller";
-import authMiddleware from "../middleware/auth.middleware";
-
-
-const router = Router();
-
-router.post("/register", register);
-router.post("/login", login);
-=======
 import authMiddleware from "../middleware/auth.middleware";
 
 import { deleteUser } from "../controller/User/deletUser.controller";
@@ -21,7 +11,6 @@ const router = Router();
 
 router.post("/auth/register", register);
 router.post("/auth/login", login)
->>>>>>> 9726a4c12b76eb8d283a1fc93101182ee6b15d5c
 router.get("/users", authMiddleware, getUsers);
 router.delete("/users/:id", authMiddleware, deleteUser);
 
