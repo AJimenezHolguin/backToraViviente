9630505a0b1526b772f47f3967d9ea2e11bbc2b5
anderson <anjimhol23@gmail.com>
2025-05-24 07:11:10 +0100

    Refactor user controller functions for registration, login, user retrieval, and deletion

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -5,15 +5,9 @@ import { Express } from "express";
 const options = {
   definition: {
     openapi: "3.0.0",
-<<<<<<< HEAD
-    info: { title: "User API", version: "1.0.0" },
-  },
-  apis: ["./src/routes/*.ts"],
-=======
     info: { title: "Tora Viviente API", version: "1.0.0" },
   },
   apis: ["./src/routes/*.ts", "./src/docs/**/*.yaml"], // Incluye el archivo YAML
->>>>>>> 9726a4c12b76eb8d283a1fc93101182ee6b15d5c
 };
 
 const swaggerSpec = swaggerJsDoc(options);
