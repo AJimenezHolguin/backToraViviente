fb61eb7a07db77f3288c69f2e3504e282007f278
AJimenezHolguin <122332192+AJimenezHolguin@users.noreply.github.com>
2025-05-25 21:57:03 +0100

    Develop (#10)
    * Refactor authentication middleware and user routes
    
    * Refactor authentication middleware and user routes for improved readability and consistency
    
    * Refactor user controller functions for registration, login, user retrieval, and deletion
    
    * Update tsconfig.json and package.json for Node.js types compatibility
    
    * Add static file serving for public directory and create index.html for initial landing page
    
    * Refactor app.ts to export the Express app for use in api.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import connectDB from "./config/db";
+// import userRoutes from "./routes/user.routes";
+// import songRoutes from "./routes/songs.routes";
+// import { setupSwagger } from "./docs/swagger";
+
+// dotenv.config();
+// const app = express();
+// app.use(express.static("public"));
+
+// app.use(cors());
+// app.use(express.json());
+
+// connectDB();
+// setupSwagger(app);
+// app.use("/api", userRoutes);
+// app.use("/api",songRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -7,6 +31,7 @@ import songRoutes from "./routes/songs.routes";
 import { setupSwagger } from "./docs/swagger";
 
 dotenv.config();
+
 const app = express();
 app.use(express.static("public"));
 
@@ -16,7 +41,8 @@ app.use(express.json());
 connectDB();
 setupSwagger(app);
 app.use("/api", userRoutes);
-app.use("/api",songRoutes);
+app.use("/api", songRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+// ❌ Ya no se usa app.listen aquí
+// ✅ Exportamos la app para usarla en api.ts
+export default app;
