6a47f88a5f2d393a26bc7d8dd1a872851e6aa46d
anderson <anjimhol23@gmail.com>
2025-05-26 03:50:45 +0100

    Refactor file paths and server setup in index.ts

--- index.ts
+++ index.ts
@@ -1,27 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import connectDB from "./config/db";
-// import userRoutes from "./routes/user.routes";
-// import songRoutes from "./routes/songs.routes";
-// import { setupSwagger } from "./docs/swagger";
-
-// dotenv.config();
-// const app = express();
-// app.use(express.static("public"));
-
-// app.use(cors());
-// app.use(express.json());
-
-// connectDB();
-// setupSwagger(app);
-// app.use("/api", userRoutes);
-// app.use("/api",songRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
-
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -31,24 +7,18 @@ import songRoutes from "./routes/songs.routes";
 import { setupSwagger } from "./docs/swagger";
 
 dotenv.config();
-
 const app = express();
-app.use(express.static("public"));
 
 app.use(cors());
+app.use(express.static("public"));
 app.use(express.json());
 
 connectDB();
 setupSwagger(app);
 app.use("/api", userRoutes);
-app.use("/api", songRoutes);
+app.use("/api",songRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
 
 
-// Ruta simple de prueba
-app.get("/ping", (_req, res) => {
-    res.send("pong");
-  });
-
-// ❌ Ya no se usa app.listen aquí
-// ✅ Exportamos la app para usarla en api.ts
-export default app;
