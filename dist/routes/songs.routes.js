2b5391cfb617a22ffd3c731d2157afbb794fdf83
anderson <anjimhol23@gmail.com>
2026-05-01 15:23:13 -0500

    refactor: extend role validation to include Musician role in playlists and songs routes

--- dist/routes/songs.routes.js
+++ dist/routes/songs.routes.js
@@ -15,10 +15,10 @@ const updateSong_controller_1 = require("../controller/songs/updateSong.controll
 const CreateSong_controllerr_1 = require("../controller/songs/CreateSong.controllerr");
 const pagination_validation_1 = require("../utils/pagination.validation");
 const router = (0, express_1.Router)();
-router.post(`/songs/create`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin]), CreateSong_controllerr_1.createSong);
-router.get(`/songs/user`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin]), getAllSongs_controller_1.getSongsValidation, pagination_validation_1.handlePaginationValidation, getSongsByUser_controller_1.getSongsByUser);
+router.post(`/songs/create`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin, auth_1.Roles.Musician]), CreateSong_controllerr_1.createSong);
+router.get(`/songs/user`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin, auth_1.Roles.Musician]), getAllSongs_controller_1.getSongsValidation, pagination_validation_1.handlePaginationValidation, getSongsByUser_controller_1.getSongsByUser);
 router.get(`/songs`, auth_middleware_1.default, getAllSongs_controller_1.getSongsValidation, pagination_validation_1.handlePaginationValidation, getAllSongs_controller_1.getAllSongs);
 router.get(`/songs/:id`, auth_middleware_1.default, getSongById_controller_1.getSongById);
-router.delete(`/songs/:id`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin]), deleteSong_controller_1.deleteMySong);
-router.put(`/songs/:id`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin]), updateSong_controller_1.updateMySong);
+router.delete(`/songs/:id`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin, auth_1.Roles.Musician]), deleteSong_controller_1.deleteMySong);
+router.put(`/songs/:id`, auth_middleware_1.default, (0, validateRole_1.validateRole)([auth_1.Roles.Admin, auth_1.Roles.Musician]), updateSong_controller_1.updateMySong);
 exports.default = router;
